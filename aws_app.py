7dace0214c25c829a536aac3da3e447e23fd19ef
Thasleem <thasleemsk88@gmail.com>
2025-07-22 18:40:24 +0530

    Update aws_app.py

--- aws_app.py
+++ aws_app.py
@@ -516,4 +516,4 @@ def check_session():
 if __name__ == '__main__':
     init_dynamodb_tables()
     webbrowser.open('http://127.0.0.1:5000')
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
