d6ccfab4c36645b6213cc4721c193e295720a91b
Thasleem <thasleemsk88@gmail.com>
2025-07-26 13:32:34 +0530

    Update aws_app.py

--- aws_app.py
+++ aws_app.py
@@ -17,7 +17,7 @@ app.secret_key = secrets.token_hex(16)
 AWS_REGION = 'us-east-1'
 AWS_ACCESS_KEY_ID = 'your_access_key'
 AWS_SECRET_ACCESS_KEY = 'your_secret_key'
-SNS_TOPIC_ARN = 'arn:aws:sns:us-east-1:235494817119:stocker'
+SNS_TOPIC_ARN = 'arn:aws:sns:us-east-1:235494817119:stocker1'
 # Initialize AWS services
 dynamodb = boto3.resource('dynamodb', region_name=AWS_REGION,
                          aws_access_key_id=AWS_ACCESS_KEY_ID,
